1982210f596c17e4a9de5d80da9b28781911688d
shimaakamal <Shimaakamalm2@gmal.com>
2025-11-12 15:30:58 +0200

    add major list to department for composition

---
+++ src/main/java/com/group/educate/educate/models/User/StudentDepartment.java
@@ -0,0 +1,39 @@
+package com.group.educate.educate.models.User;
+
+import java.util.ArrayList;
+
+public class StudentDepartment {
+    private String departmentId;
+    private String departmentName;
+    private ArrayList<StudentMajor> majors;
+
+    public StudentDepartment(String departmentId, String departmentName) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.majors = new ArrayList<>();
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public ArrayList<StudentMajor> getMajors() {
+        return majors;
+    }
+
+    public void setMajors(ArrayList<StudentMajor> majors) {
+        this.majors = majors;
+    }
+}
