fa2b06eec8dd5aa1042aedf8752f1581d564b8e2
shimaakamal <Shimaakamalm2@gmal.com>
2025-11-12 15:51:14 +0200

    Nothing

---
+++ src/main/java/com/group/educate/educate/models/User/StudentMajor.java
@@ -0,0 +1,9 @@
+package com.group.educate.educate.models.User;
+
+public class StudentMajor {
+    private String majorId;
+    private String majorName;
+    // student makes a application so create method to make or submit
+    //application
+
+}
