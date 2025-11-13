74e6e672dfebb34e3a5c09611642a4b994cbb9ea
shimaakamal <Shimaakamalm2@gmal.com>
2025-11-13 17:21:56 +0200

    made a method to add students to major (one major has many students)

--- src/main/java/com/group/educate/educate/Model/User/Student/StudentMajor.java
+++ src/main/java/com/group/educate/educate/Model/User/Student/StudentMajor.java
@@ -1,12 +1,23 @@
 package com.group.educate.educate.Model.User.Student;
 
+import java.util.ArrayList;
+
 public class StudentMajor {
     private String majorId;
     private String majorName;
+    // major has many students
+    private ArrayList<Student> students= new ArrayList();
 
     public StudentMajor(String majorId, String majorName) {
         this.majorId = majorId;
         this.majorName = majorName;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+    public void addStudents(Student student) {
+        students.add(student);
     }
 
     public String getMajorId() {
