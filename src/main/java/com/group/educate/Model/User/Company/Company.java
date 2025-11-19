74f48dad1a7df1a80c580ff571113d75a0ea6173
shimaakamal <Shimaakamalm2@gmal.com>
2025-11-19 14:50:46 +0200

    company repo (not fully finished ) small edit in company class

--- src/main/java/com/group/educate/Model/User/Company/Company.java
+++ src/main/java/com/group/educate/Model/User/Company/Company.java
@@ -1,5 +1,7 @@
 package com.group.educate.Model.User.Company;
 
+import com.group.educate.Model.User.UserRole;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,12 +10,14 @@ public class Company {
     private String name;
     //type of id is not decided
     private String websiteURL;
+    private UserRole role;
     private static List<Recruiter> recruiters = new ArrayList<>();
 
-    public Company(String industry, String name, String websiteURL) {
+    public Company(String industry, String name, String websiteURL, UserRole role) {
         this.industry = industry;
         this.name = name;
         this.websiteURL = websiteURL;
+        this.role = role;
     }
 
     public String getIndustry() {
@@ -40,12 +44,13 @@ public class Company {
         this.websiteURL = websiteURL;
     }
 
-    public static List<Recruiter> getRecruiters() {
-        return recruiters;
-    }
 
     public static void setRecruiters(List<Recruiter> recruiters) {
         Company.recruiters = recruiters;
+    }
+
+    public UserRole getRole() {
+        return role;
     }
 
     public static void addRecruiter(Recruiter recruiter) {
---
+++ src/main/java/com/group/educate/Repo/CompanyRepository.java
@@ -0,0 +1,63 @@
+package com.group.educate.Repo;
+
+import com.group.educate.Model.User.Company.Company;
+import com.group.educate.Model.User.UserRole;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CompanyRepository {
+    private final String path = "data/companies.txt";
+
+    public CompanyRepository() {
+        File file = new File(path);
+        if (!file.exists()) file.mkdirs();
+    }
+
+    private String formatCompany(Company company) {
+        return company.getName() + "|" +
+                company.getIndustry() + "|" +
+                company.getWebsiteURL() + "|" +
+                company.getRole() + "|";
+    }
+
+    public void save(Company company) {
+        String line = formatCompany(company);
+        writeToFile(path, line, true);
+    }
+
+    private void writeToFile(String path, String line, boolean append) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, append))) {
+            writer.write(line);
+            writer.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<String> readFromFile(String path) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+        }
+        return lines;
+    }
+    private Company parseCompany(String line) {
+        String[] parts = line.split("\\|");
+        int companyId=Integer.parseInt(parts[0]);
+        String industry = parts[1];
+        String name = parts[2];
+        String websiteURL = parts[3];
+        String role = parts[4];
+
+        return new Company(parts[1],parts[2],parts[3],UserRole.COMPANY);
+    }
+}
+// i didnt add find methods yet am aware
+
+
