e9e37686dc0d2e28ab578dcb627530a6c21722f7
Ziad <mziad039@gmail.com>
2025-11-20 20:17:15 +0200

    Minor changes

--- src/main/java/com/group/educate/Repo/FileManager.java
+++ src/main/java/com/group/educate/Repo/FileManager.java
@@ -1,21 +1,22 @@
 package com.group.educate.Repo;
 
 import com.group.educate.Model.Roadmap.Skill.Skill;
-import com.group.educate.Model.User.Company.Recruiter;
-import com.group.educate.Model.User.UserRole;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager {
 
     protected static void write(String fileName) throws Exception {
         FileOutputStream stream = new FileOutputStream(fileName);
         ObjectOutputStream objectStream = new ObjectOutputStream(stream);
-        objectStream.writeObject(new Skill("Hello", "Hi", null));
 
+        objectStream.writeObject(new Skill("Swift Student Challenge", "Empty", new ArrayList<>(List.of(new URL[]{new URL("https://developer.apple.com")}))));
     }
 
     protected static void read(String fileName) throws Exception {
