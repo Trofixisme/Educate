4398c2457ffcae40537b8c7a1e9aec68f8a1f404
Ziad <mziad039@gmail.com>
2025-11-20 23:50:55 +0200

    Minor changes to BaseRepository.java

--- src/main/java/com/group/educate/Repo/BaseRepository.java
+++ src/main/java/com/group/educate/Repo/BaseRepository.java
@@ -1,10 +1,18 @@
 package com.group.educate.Repo;
 
-import java.io.BufferedWriter;
 import java.io.*;
 import java.util.*;
 
-public  class BaseRepository {
+public class BaseRepository extends FileManager {
+
+    public static <T> T findbyObject(String fileName, T object) throws Exception {
+        for (Object t : FileManager.read(object.getClass(), fileName)) {
+            if (t.equals(object)) {
+                return (T) t;
+            }
+        }
+        return null;
+    }
 
 
 }
--- src/main/java/com/group/educate/Repo/FileManager.java
+++ src/main/java/com/group/educate/Repo/FileManager.java
@@ -14,7 +14,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public final class FileManager {
+public class FileManager {
 
     protected static <T> void write(String fileName, T... objects) throws Exception {
         FileOutputStream stream = new FileOutputStream(fileName);
