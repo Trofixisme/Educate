1c5221dd22b6ccf03682bf737582666bd01c9229
sandraremon <123971540+sandraremon@users.noreply.github.com>
2025-11-25 22:33:20 +0200

    controllers and html no errors but postMapping is not working

--- src/main/java/com/group/educate/Contoller/UserController.java
+++ src/main/java/com/group/educate/Contoller/UserController.java
@@ -1,9 +1,7 @@
 // src/main/java/com/group/educate/Web/HomeController.java
 package com.group.educate.Contoller;
 
-import com.group.educate.Model.User.Student;
 import com.group.educate.Model.User.User;
-import com.group.educate.Model.User.UserRole;
 import com.group.educate.Services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,40 +15,38 @@ public class  UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/register")
+    @GetMapping("/")
     public String showRegisterPage(Model model) {
-        model.addAttribute("user", new Student("Ziad", "Ali", "ziad@example.com",
-                "password123", UserRole.STUDENT, 2024, "Cairo University",
-                "Computer Science", "FCAI"));
-        return "redirect:/login";  // corresponds to register.html
+        model.addAttribute("user", new User());
+        return "register_form";
     }
 
-    @PostMapping("/register")
-    public String registerUser(@ModelAttribute("user") User user) {
+    @PostMapping("/users/register")
+    public String registerUser(@ModelAttribute("user") User user, Model model) {
         try {
             userService.register(user);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            model.addAttribute("errorMessage", e.getMessage());
+            // 2. Return the view name (DO NOT REDIRECT)
+            return "register_form";
         }
+        // Only redirect on SUCCESS
         return "redirect:/login";
     }
 
     @GetMapping("/login")
     public String showloginPage(Model model) {
-        model.addAttribute("user", new Student("Ziad", "Ali", "ziad@example.com",
-                "password123", UserRole.STUDENT, 2024, "Cairo University",
-                "Computer Science", "FCAI"));
-        return "redirect:/";
+        return "login";
     }
 
     @PostMapping("/login")
     public String login(@ModelAttribute("user") User user) {
         try {
-            userService.register(user);
+            userService.login(user.getEmail(),user.getPlainPassword());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-        return "redirect:/";
+        return "redirect:/register_form";
     }
 
 }
--- src/main/java/com/group/educate/Model/User/User.java
+++ src/main/java/com/group/educate/Model/User/User.java
@@ -4,23 +4,26 @@ package com.group.educate.Model.User;
 
 import org.mindrot.jbcrypt.BCrypt;
 import java.io.Serializable;
-import java.util.Objects;
 import java.util.UUID;
 
-public abstract class User implements Serializable {
+public class User implements Serializable {
 
-    private String hashedPassword;
+    private String plainPassword;
     private String fname;
     private String lname;
     private String email;
     private final UUID userID;
     private final UserRole role;
+    public User(){
+        userID = UUID.randomUUID();
+        this.role = UserRole.STUDENT;
+    }
 
     public User(String fname, String lname, String email, String plainPassword, UserRole role) {
         userID = UUID.randomUUID();
         hashPassword(plainPassword);
-        setFName(fname);
-        setLName(lname);
+        setFname(fname);
+        setLname(lname);
         setEmail(email);
         this.role = role;
     }
@@ -28,50 +31,51 @@ public abstract class User implements Serializable {
     public User(String userID, String fname, String lname, String email, String plainPassword, UserRole role) {
         this.userID = UUID.fromString(userID);
         hashPassword(plainPassword);
-        setFName(fname);
-        setLName(lname);
+        setFname(fname);
+        setLname(lname);
         setEmail(email);
         this.role = role;
     }
 
     //Hash password with BCrypt
-    public void hashPassword(String plainPassword) { hashedPassword = BCrypt.hashpw(plainPassword, BCrypt.gensalt(12)); }
+    public void hashPassword(String plainPassword) { this.plainPassword = BCrypt.hashpw(plainPassword, BCrypt.gensalt(12)); }
 
     //Check if the input password matches the stored hash
     public boolean verifyPassword(String plainPassword) {
-        return BCrypt.checkpw(plainPassword, this.hashedPassword);
+        return BCrypt.checkpw(plainPassword, this.plainPassword);
     }
 
-    public String getHashedPassword() {
-        return hashedPassword;
+    public String getPlainPassword() {
+        return plainPassword;
     }
 
-    public String getUserID() {
-        return userID.toString();
+    public void setPlainPassword(String plainPassword) {
+        this.plainPassword = plainPassword;
     }
 
     public String getFname() {
         return fname;
     }
 
-    //TODO: Also add some validation when setting the user's name (Why do we even need to do that??)
-
-    public void setFName(String fName) {
-        this.fname = fName;
+    public void setFname(String fname) {
+        this.fname = fname;
     }
 
     public String getLname() {
         return lname;
     }
 
-    public void setLName(String lName) {
-        this.lname = lName;
+    public void setLname(String lname) {
+        this.lname = lname;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public UUID getUserID() {
+        return userID;
+    }
     //TODO: do some ACTUAL validation before changing the user's email
 
     public void setEmail(String email) {
@@ -92,7 +96,7 @@ public abstract class User implements Serializable {
                 getFname() + "|" +
                 getLname() + "|" +
                 getEmail() + "|" +
-                getHashedPassword() + "|" +
+                getPlainPassword() + "|" +
                 getRole() + "|";
     }
 
